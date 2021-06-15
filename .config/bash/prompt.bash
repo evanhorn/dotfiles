70ea22125025f1fd19d72f385f7ca8f9f57c331d
Eric Van Horn <ericmvanhorn7@gmail.com>
2021-06-15 09:45:42 -0500

    use powerline from local site-packages

--- .config/bash/prompt.bash
+++ .config/bash/prompt.bash
@@ -1,7 +1,9 @@
 # use POWERLINE
-if [ -f `which powerline-daemon` ]; then
-  powerline-daemon -q
+if [ -f $HOME/.local/bin/powerline-daemon ]; then
+  $HOME/.local/bin/powerline-daemon -q
   POWERLINE_BASH_CONTINUATION=1
   POWERLINE_BASH_SELECT=1
-  . /usr/share/powerline/bindings/bash/powerline.sh
+
+  export POWERLINE_COMMAND=$HOME/.local/bin/powerline
+  source "`python -m site --user-site`/powerline/bindings/bash/powerline.sh"
 fi
