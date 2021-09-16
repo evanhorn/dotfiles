6aa13c9529d828c77c594e5d6c88691884cb027a
Eric Van Horn <ericmvanhorn7@gmail.com>
2021-09-16 14:13:17 -0500

    changed to use system FZF rather than vim version

--- .config/fzf/fzf.bash
+++ .config/fzf/fzf.bash
@@ -1,13 +1,10 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/evanhorn/.local/share/nvim/plugged/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/evanhorn/.local/share/nvim/plugged/fzf/bin"
-fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/evanhorn/.local/share/nvim/plugged/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/evanhorn/.local/share/nvim/plugged/fzf/shell/key-bindings.bash"
+source "/usr/share/doc/fzf/examples/key-bindings.bash"
