c950e19a00ee60d7295d1e53b27faccfe60e1610
Eric Van Horn <ericmvanhorn7@gmail.com>
2021-10-01 15:25:49 -0500

    reverted yet again to pipx for powerline-status as powerline is non-functional in neovim

--- .config/bash/prompt.bash
+++ .config/bash/prompt.bash
@@ -1,9 +1,10 @@
 # use POWERLINE
+export POWERLINE_PATH="$HOME/.local/pipx/venvs/powerline-status/lib/python3.8/site-packages/powerline"
+
 if [ -f $HOME/.local/bin/powerline-daemon ]; then
   $HOME/.local/bin/powerline-daemon -q
   POWERLINE_BASH_CONTINUATION=1
   POWERLINE_BASH_SELECT=1
 
-  export POWERLINE_COMMAND=$HOME/.local/bin/powerline
-  source "`python -m site --user-site`/powerline/bindings/bash/powerline.sh"
+  source "$POWERLINE_PATH/bindings/bash/powerline.sh"
 fi
