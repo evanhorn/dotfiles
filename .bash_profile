051fc84d46330ade135baa35cb85308c5944e14c
Eric Van Horn <ericmvanhorn7@gmail.com>
2021-12-09 12:46:42 -0600

    moved path settings to bootstrap; cleaned up profile/bash files

--- .bash_profile
+++ .bash_profile
@@ -1,22 +1,10 @@
 # .bash_profile
 
-# Get the aliases and functions
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
 if [ -f ~/.bashrc ]; then
-	. ~/.bashrc
+  . ~/.bashrc
 fi
 
-# User specific environment and startup programs
-
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
-fi
-
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/.local/bin" ] ; then
-    PATH="$HOME/.local/bin:$PATH"
-fi
-
-# export PATH
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+eval "$(pyenv init --path)"
--- .config/shell/bootstrap.sh
+++ .config/shell/bootstrap.sh
@@ -1,1 +1,23 @@
-path_append "$HOME/.local/bin"
+# set PATH so it includes user's private bin if it exists
+
+if [ -d "$HOME/.nvm" ] ; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
+if [ -s "$HOME/.cargo/env" ] ; then
+  source "$HOME/.cargo/env"
+fi
+
+for LOCAL_PATH in ".rvm/bin" "bin" ".local/bin"; do
+  if [ -d "$HOME/$LOCAL_PATH" ] ; then
+    path_prepend "$HOME/$LOCAL_PATH"
+  fi
+done
+
+# So pyenv gets recognized
+eval "$(pyenv init -)"
+
+# Load pyenv-virtualenv automatically
+eval "$(pyenv virtualenv-init -)"
