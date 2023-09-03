5ad6844a26d154344b1a77af6e1970e13f61941d
Eric Van Horn <eric.vanhorn@pm.me>
2023-09-03 15:06:30 -0500

    updated colorscheme to base16 for terminal and neovim

--- .config/nvim/lua/user/plugins/after.lua
+++ .config/nvim/lua/user/plugins/after.lua
@@ -1,6 +1,5 @@
 -- vim: set foldmarker={,} foldlevel=0 foldmethod=marker:
 
-require('user.plugins.airline_after')
 require('user.plugins.treesitter')
 require('user.plugins.lsp')
 require('user.plugins.telescope_trouble')
